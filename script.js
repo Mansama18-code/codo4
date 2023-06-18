5f500bf09ae31d6f754faeb8a8865a5f077cebb4
Manuel Samaniego <mansamaniego@gmail.com>
2023-06-18 20:13:49 -0300

    Integrador de JS corregido

--- script.js
+++ script.js
@@ -2,14 +2,37 @@ function borrar() {
     document.getElementById("myForm").reset();
 }
 
-function resumen(){
-    var costo = 200;
+function verificar() {
+    /* Nombre */
+    var resp = false;
 
+    if (document.getElementById("nombre-form").value == "" && document.getElementById("apellido-form").value == "" && document.getElementById("celectro").value == "" && document.getElementById("cuantos").value == "") {
+        resp = true;
+        
+    }
+    
+    return resp;
+}
+    
+     
+
+function resumen(){
+    var ver = verificar();
+    
+    if (ver == true) {
+            document.getElementById("obligatorio").style.display = "flex";
+        }
+
+
+
+    var costo = 200;
     var dato = document.getElementById("selecto");
     var dato2 = dato.value;
     var asistencia = document.getElementById("cuantos");
     var asistencia2 = asistencia.value;
     var total;
+
+
     if ( dato2 == "Estudiante") {
         costo = costo - costo * 0.80; 
     } else if (dato2 == "Trainne") {
@@ -23,9 +46,3 @@ function resumen(){
     return document.getElementById("resumende").innerHTML=total;
 }
 
-/*
-
-    total = asistencia * costo;
-
-    
-*/
